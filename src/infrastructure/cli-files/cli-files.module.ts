0e339aa1bee8b5bc98bcbca889b1d9ec50582f8e
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-05-12 21:12:29 -0400

    fix: Elimina CryptoModule de las importaciones del módulo CliFiles y lo añade al módulo ApiKey para que pueda ser inyectado en el caso de uso

--- src/infrastructure/cli-files/cli-files.module.ts
+++ src/infrastructure/cli-files/cli-files.module.ts
@@ -4,7 +4,6 @@ import { createDynamoCliFilesRepository } from '@infrastructure/cli-files/cli-fi
 import { CliFilesRepository } from '@titvo/trigger'
 import { GetCliFilesSignedUrlsUseCase } from '@trigger/app/cli-files/cli-files.service'
 import { ApiKeyModule } from '@infrastructure/api-key/api-key.module'
-import { CryptoModule } from '@infrastructure/crypto/crypto.module'
 @Module({
   providers: [
     GetCliFilesSignedUrlsUseCase,
@@ -21,7 +20,7 @@ import { CryptoModule } from '@infrastructure/crypto/crypto.module'
       }
     }
   ],
-  imports: [StorageModule, ApiKeyModule, CryptoModule],
+  imports: [StorageModule, ApiKeyModule],
   exports: [GetCliFilesSignedUrlsUseCase]
 })
 export class CliFilesModule {}
